7f79b063af2ec171cae05e3ad921fc7cdc020b16
sawyerhalverson <shalverson321@gmail.com>
2023-03-21 11:32:07 -0600

    finall mission12

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+//create the header
+function Header() {
+  return (
+    <header>
+      <h1>College Basketball Teams</h1>
+    </header>
+  );
+}
+
+
+// create teamProps function and interface
+interface TeamProps {
+  name: string;
+  mascot: string;
+  city: string;
+  state: string;
+}
+
+function TeamCard(props: TeamProps) {
+  const { name, mascot, city, state } = props;
+  return (
+    <div className="team-card">
+      <h2>{name}</h2>
+      <p>Mascot: {mascot}</p>
+      <p>Location: {city}, {state}</p>
+    </div>
+  );
+}
+
+
+import teamsData from './teamsData.json';
+
+
+
+//create the actual app that lists stuff out
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="team-cards">
+        {teamsData.teams.map((team, index) => (
+          <React.Fragment key={team.tid}>
+            <TeamCard
+              name={team.school}
+              mascot={team.name}
+              city={team.city}
+              state={team.state}
+            />
+            {index !== teamsData.teams.length - 1 && <br />}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
